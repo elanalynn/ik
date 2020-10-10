1a1bbd6605e8bd7490bb1946794923fe685e94f6
Elana Kopelevich <ekopelevich@gmail.com>
2020-10-09 20:01:22 -0600

    adjust header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,37 +6,39 @@ import Badge from './Badge';
 
 export default function Header({ onAction = () => {} }) {
   return (
-    <header id="header">
-      <h1>
-        <Link to="/">
-          <strong>{config.heading}</strong>
-        </Link>
-      </h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/" className="">
-              Gallery
-            </Link>
-          </li>
-          <li>
-            <Link to="/about" className="">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="/cart" className="">
-              Cart
-              <Badge />
-            </Link>
-          </li>
-          <li>
-            <a href="#footer" onClick={e => onAction(e)}>
-              Contact
-            </a>
-          </li>
-        </ul>
-      </nav>
+    <header className="header">
+      <section className="header-content">
+        <h1>
+          <Link to="/">
+            <strong>{config.heading}</strong>
+          </Link>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" className="">
+                Gallery
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/cart" className="">
+                Cart
+                <Badge />
+              </Link>
+            </li>
+            <li>
+              <a href="#footer" onClick={e => onAction(e)}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </section>
     </header>
   );
 }
